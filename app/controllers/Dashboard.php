8b0d28b4772a0b5f837f671b3ed4361824185cbe
nadyafatyana <107346515+nadyafatyana@users.noreply.github.com>
2022-06-21 23:55:44 +0700

    Create Dashboard.php

---
+++ app/controllers/Dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+
+class Dashboard extends Controller
+{
+
+	public function index()
+	{
+		$data['row_index'] = "Ini file app/controllers/Dashboard.php - index()";
+		$this->dashboard('dashboard/index', $data);
+	}
+	public function logout()
+	{
+		$this->home('home/index');
+	}	
+}
